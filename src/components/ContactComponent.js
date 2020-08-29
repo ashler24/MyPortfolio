45a5f510e99c307d0ee8c72716e81a5da081c8a1
ashler24 <43813305+ashler24@users.noreply.github.com>
2020-08-30 02:30:35 +0530

    contact and biodata changed

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -11,7 +11,15 @@ function Contact() {
                             <CardTitle>Contact Me</CardTitle>
                             <CardBody>
                                 <CardText>
-                                    ....
+                                    <a target="_blank" href="https://twitter.com/pawar2467">
+                                        <img align="left" alt="Abhijeet Pawar | Twitter" width="25px" src="https://raw.githubusercontent.com/Akshay2996/Akshay2996/master/assets/twitter.png" />
+                                    </a>
+                                    <a target="_blank" href="https://www.instagram.com/lone_w_olf/">
+                                        <img align="left" alt="Abhijeet Pawar | Instagram" width="25px" src="https://raw.githubusercontent.com/Akshay2996/Akshay2996/master/assets/instagram.png" />
+                                    </a>
+                                    <a target="_blank" href="https://www.linkedin.com/in/abhijeet-pawar-b96237130/">
+                                        <img align="left" alt="Abhijeet Pawar | LinkedIn" width="25px" src="https://raw.githubusercontent.com/Akshay2996/Akshay2996/master/assets/linkedin.png" />
+                                    </a>
                                 </CardText>
                             </CardBody>
                         </Card>
