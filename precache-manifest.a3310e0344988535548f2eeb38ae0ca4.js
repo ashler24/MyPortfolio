c40cee13f555e7505335081219d93e0b14622d2f
ashler24 <43813305+ashler24@users.noreply.github.com>
2020-08-30 01:16:57 +0530

    Updates

---
+++ precache-manifest.a3310e0344988535548f2eeb38ae0ca4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "015e9f6daed2c595e35d4479fabb8ccf",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "a5c6942305cb19f51e9f",
+    "url": "/MyPortfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "36e2adbcae16d8189b67",
+    "url": "/MyPortfolio/static/css/main.f7872e59.chunk.css"
+  },
+  {
+    "revision": "a5c6942305cb19f51e9f",
+    "url": "/MyPortfolio/static/js/2.4297e961.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MyPortfolio/static/js/2.4297e961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e2adbcae16d8189b67",
+    "url": "/MyPortfolio/static/js/main.616d96de.chunk.js"
+  },
+  {
+    "revision": "0d8bee11d1469486e213",
+    "url": "/MyPortfolio/static/js/runtime-main.7e73d9a0.js"
+  }
+]);
